13d572ebcd754c57ecb5dfc4499f145a295e76fa
hacktea8 <admin@hacktea8.com>
2013-09-29 19:26:04 +0800

    update Imglib

--- application/libraries/Imglib.php
+++ application/libraries/Imglib.php
@@ -2,52 +2,53 @@
 class Imglib{
   protected static $im=0;
   protected $thumb=0;
-  public static $imgdata='';
+  protected $imgdata='';
   protected $imgtype='';
   protected $sizeinfo='';
-  public static $config=array();
-  public static $watermark='';
-  public static $postion='';
-  public static $waterfont='';
+  public $config=array();
+  public $watermark='';
+  public $postion='';
+  public $waterfont='';
   public static $mass=80;//80% 图片质量
-  public static $dstimg=NULL;
+  public $dstimg=NULL;
   public static $iscut=0;//是否切图
   public static $wsize=array();//水印图大小
-  public static $size=array();//原图大小
-  public static $resize=array();//缩略大小
+  public $size=array();//原图大小
+  public $resize=array();//缩略大小
 
 //检查环境
-  public static function checksupport(){
-     if(function_exists(gd_info)){
+  public function checksupport(){
+     if(function_exists('gd_info')){
         return gd_info();
      }
      return false;
   }
 //初始化配置
-  public static function init(){
-    if(isset($config['imgdata'])){
-       $this->imgdata=$config['imgdata'];
+  public function init(){
+    if(isset($this->config['imgdata'])){
+       $this->imgdata=$this->config['imgdata'];
        $this->im=imagecreatefromstring($this->imgdata);
+//var_dump($this->im);exit;
     }
-    if(isset($config['imgtype'])){
-       $this->imgtype='image'.strtolower($config['imgtype']);
+    if(isset($this->config['imgtype'])){
+       $this->imgtype='image'.strtolower($this->config['imgtype']);
     }
-    if(isset($config['watermark'])){
-       $this->watermark=$config['watermark'];
+    if(isset($this->config['watermark'])){
+       $this->watermark=$this->config['watermark'];
        $this->wim=imagecreatefromstring($this->watermark);
     }
-    if(isset($config['postion'])){
-       $this->postion=$config['postion'];
+    if(isset($this->config['postion'])){
+       $this->postion=$this->config['postion'];
     }
 	$this->getimgsizefromstring();
   }
   
 //从内容获取图片句柄
-  public static function getimgsizefromstring(){
+  public function getimgsizefromstring(){
      $this->sizeinfo=getimagesizefromstring($this->imgdata);
   }
 //增加缩略图
-  public static function getthumbimg(){
+  public function getthumbimg(){
         //改变后的图象的比例
         $resize_ratio = ($this->resize['width'])/($this->resize['height']);
         //实际图象的比例
@@ -85,12 +86,12 @@ class Imglib{
        // ImageJpeg ($newimg,$this->dstimg);
   }
 //保存图片
-  public static function saveimg(){
+  public function saveimg(){
      imagejpeg($this->im, $this->dstimg, $this->mass);
      imagedestroy($this->im);
   }
 //增加水印
-  public static function setwatermark(){
+  public function setwatermark(){
      $watersize=getimagesizefromstring($this->watermark);
      $wim=imagecreatefromstring($this->watermark);
      $w_w=0;$posX=0;
@@ -101,7 +102,7 @@ class Imglib{
      switch($this->postion){
 //中间
      case 0:
-       $posX=($this->size['width']-$w_w)/2
+       $posX=($this->size['width']-$w_w)/2;
        $posY=$this->size['height']/2;
      break;
 //左上角
@@ -128,13 +129,19 @@ class Imglib{
      imagecopy($this->im, $this->wim, $posX, $posY, 0, 0, $w_w,$w_h);//拷贝水印到目标文件  
   }
 //展示图片
-  public static function showimg(){
+  public function showimg(){
+     //ob_start();
+     //ob_clean();
+//var_dump($this->sizeinfo);exit;
+//var_dump($this->imgtype);exit;
      header("Content-Type: ".$this->sizeinfo['mime']);
-     ob_clean();
+echo $this->imgdata;exit;
+/*
      $this->imgtype($this->im);
      if(is_resource($this->im)){
          imagedestroy($this->im);
      }
+*/
   }
 }
 ?>
