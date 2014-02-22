b81ad41ebc0277b3478945d6fc6c7ced9708b895
hacktea8 <pic@hacktea8.com>
2014-02-22 14:38:29 +0800

    add uc_client

---
+++ config_ucenter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'a0325080729');
+define('UC_DBPW', 'molong1992JIB');
+define('UC_DBNAME', 'a0325080729');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`a0325080729`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', 'w81dS0Reb372p3L038g6H2S6odm3n5q5C6Rfdee8PfF9Sfc6Wex2ga1aceO579K7');
+define('UC_API', 'http://www.hacktea8.com/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
