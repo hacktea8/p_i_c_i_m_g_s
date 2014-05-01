f03ff09ba36178c6d62819a0bdbbdc4b07204fb7
hacktea8 <admin@hacktea8.com>
2014-05-01 15:55:44 +0800

    add download file

---
+++ showfile.php
@@ -0,0 +1,15 @@
+<?php
+$key=isset($_GET['key'])?trim($_GET['key']):'';
+if(!$key){
+header("HTTP/1.1 404 Not Found");  
+header("Status: 404 Not Found");  
+exit;
+//   $key = '3958009_0000671092.jpg';
+}
+define('SHOW_IMG',1);
+$root=dirname(__FILE__);
+$imgpath='/apps/picimgs/admattach/';
+$ext = strrchr($key,'.');
+//echo $ext;exit;
+require_once($root.'/file.php');
+?>
